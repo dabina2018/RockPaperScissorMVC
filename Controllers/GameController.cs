3f5f04e84cd0b3653578660b937e55c97d53ef3d
dabina2018 <daveena.cooper@outlook.com>
2020-04-25 23:36:02 -0700

    updating project with initial routing completed

---
+++ Controllers/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVC.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult GameBoard()
+        {
+            return View();
+        }
+
+        
+        public IActionResult EnterPlayerData()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Models
+{
+    public class Player
+    {
+        public static string userName = "Your Name";
+        public string userEmail = "you@domain.com";
+    }
+}
